8baa0172485132f03e16b74698041262560e5187
QuangQui2002 <dlquangqui@gmail.com>
2026-05-24 09:51:24 +0700

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Kết nối tới API của Express Backend chạy ở cổng 3000
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // baseURL: 'https://lesson-outline.onrender.com/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://lesson-outline.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
